467fb2245f40c23ef411361c59631de0754f1ae3
madokaminami <55927087+madokaminami@users.noreply.github.com>
2019-10-11 11:59:27 -0700

    Add files via upload

--- UCI-CIFAR-100/models/efficientnet.py
+++ UCI-CIFAR-100/models/efficientnet.py
@@ -3,6 +3,18 @@ import math
 import torch
 from torch import nn
 from torch.autograd import Variable
+# from .utils import load_state_dict_from_url
+
+# model_urls = {
+#     'efficientnet_b0': 'https://www.dropbox.com/s/9wigibun8n260qm/efficientnet-b0-4cfa50.pth?dl=1',
+#     'efficientnet_b1': 'https://www.dropbox.com/s/6745ear79b1ltkh/efficientnet-b1-ef6aa7.pth?dl=1',
+#     'efficientnet_b2': 'https://www.dropbox.com/s/0dhtv1t5wkjg0iy/efficientnet-b2-7c98aa.pth?dl=1',
+#     'efficientnet_b3': 'https://www.dropbox.com/s/5uqok5gd33fom5p/efficientnet-b3-bdc7f4.pth?dl=1',
+#     'efficientnet_b4': 'https://www.dropbox.com/s/y2nqt750lixs8kc/efficientnet-b4-3e4967.pth?dl=1',
+#     'efficientnet_b5': 'https://www.dropbox.com/s/qxonlu3q02v9i47/efficientnet-b5-4c7978.pth?dl=1',
+#     'efficientnet_b6': None,
+#     'efficientnet_b7': None,
+# }
 
 params = {
     'efficientnet_b0': (1.0, 1.0, 224, 0.2),
@@ -170,10 +182,11 @@ def Arctransf(feat, s=5.):
     output *= s    
     return output
 
-class Margloss(nn.Module):  
+class Margloss(nn.Module):
+    # implementation of additive margin softmax loss in https://arxiv.org/abs/1801.05599    
     def __init__(self,  s=5.):
         super(Margloss, self).__init__()
-        self.s = s 
+        self.s = s # scalar value default is 64, see normface https://arxiv.org/abs/1704.06369
     def forward(self, feat, label):
         nB = len(feat)
         output = feat * 1.0 # a little bit hacky way to prevent in_place operation on cos_theta
@@ -182,19 +195,128 @@ class Margloss(nn.Module):
         out = loss(output, label)
         return out   # return out--loss
 
+class Arcloss(nn.Module):
+    # implementation of additive margin softmax loss in https://arxiv.org/abs/1801.05599    
+    def __init__(self,  s=5., m=0.0):
+        super(Arcloss, self).__init__()
+        self.m = m # the margin value, default is 0.5
+        self.s = s # scalar value default is 64, see normface https://arxiv.org/abs/1704.06369
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+        self.mm = self.sin_m * m  # issue 1
+        self.threshold = math.cos(math.pi - m)
+    def forward(self, feat, label):
+        # print(label)
+        nB = len(feat)
+        cos_theta_2 = torch.pow(feat, 2)
+        sin_theta_2 = 1 - cos_theta_2
+        sin_theta = torch.sqrt(sin_theta_2)
+        cos_theta_m = (feat * self.cos_m - sin_theta * self.sin_m)
+        # this condition controls the theta+m should in range [0, pi]
+        #      0<=theta+m<=pi
+        #     -m<=theta<=pi-m
+        cond_v = feat - self.threshold
+        # print(cond_v)
+        cond_mask = cond_v <= 0
+        keep_val = (feat - self.mm) # when theta not in [0,pi], use cosface instead
+        cos_theta_m[cond_mask] = keep_val[cond_mask]
+        output = feat * 1.0 # a little bit hacky way to prevent in_place operation on cos_theta
+        idx_ = torch.arange(0, nB, dtype=torch.long)
+        output[idx_, label] = cos_theta_m[idx_, label]
+        output *= self.s # scale up in order to make softmax work, first introduced in normface
+        # print(output)
+        loss = nn.CrossEntropyLoss()
+        out = loss(output, label)
+        return out   # return out--loss
+
+class CosLoss(nn.Module):
+    def __init__(self, num_cls=100, s=5, alpha=0.1):
+        super(CosLoss, self).__init__()
+        self.num_cls = num_cls
+        self.alpha = alpha
+        self.scale = s
+        self.phi=nn.Parameter(torch.Tensor(1))
+        self.phi.data.uniform_(s, -s)
+
+    def forward(self, feat, y):
+        y = y.view(-1, 1)
+        batch_size = feat.size()[0]
+        feat = feat + self.phi.expand_as(feat)
+        margin_xw_norm = feat - self.alpha
+        y_onehot = torch.Tensor(batch_size, self.num_cls).cuda()
+        y_onehot.zero_()
+        y_onehot.scatter_(1, y.data.view(-1, 1), 1)
+        y_onehot.byte()
+        y_onehot = Variable(y_onehot, requires_grad=False)
+        value = self.scale * where(y_onehot, margin_xw_norm, feat)
+        # value = value
+        # logpt = F.log_softmax(value)
+        y = y.view(-1)
+        loss = nn.CrossEntropyLoss()
+        output = loss(value, y)
+        # loss = loss.mean()
+        return output
+
+class mCosLoss(nn.Module):
+    def __init__(self, num_cls=100, s=2, alpha=0.1):
+        super(mCosLoss, self).__init__()
+        self.num_cls = num_cls
+        self.alpha = alpha
+        self.scale = s
+        self.phi=nn.Parameter(torch.Tensor(1))
+        self.phi.data.uniform_(s, -s)###
+
+    def forward(self, feat, y):
+        y = y.view(-1, 1)
+        batch_size = feat.size()[0]
+        feat = feat + self.phi.expand_as(feat)
+        margin_xw_norm_h = feat - self.alpha 
+        margin_xw_norm_l = feat + self.alpha
+        y_onehot = torch.Tensor(batch_size, self.num_cls).cuda()
+        y_onehot.zero_()
+        y_onehot.scatter_(1, y.data.view(-1, 1), 1)
+        y_onehot.byte()
+        y_onehot = Variable(y_onehot, requires_grad=False)
+        value = self.scale * where(y_onehot, margin_xw_norm_h, margin_xw_norm_l)
+        # value = value
+        # logpt = F.log_softmax(value)
+        y = y.view(-1)
+        loss = nn.CrossEntropyLoss()
+        output = loss(value, y)
+        # loss = loss.mean()
+        return output
 ###############################
 
 class EfficientNet(nn.Module):
 
-    LR_REGIME = [1, 25, 0.05, 26, 50, 0.005, 51, 65, 0.0005, 66, 70, 0.00005]
+    # LR_REGIME = [1, 25, 0.05, 26, 40, 0.005, 41, 50, 0.0005, 51, 55, 0.00005]
 
-    def __init__(self, width_mult=1.0, depth_mult=1.0, dropout_rate=0.2, num_classes=100, coslinear=True):
+    LR_REGIME = [1, 25, 0.07, 26, 50, 0.007, 51, 65, 0.0007, 66, 70, 0.00007]
+
+    def __init__(self, width_mult=1.0, depth_mult=1.0, dropout_rate=0.2, num_classes=100, coslinear=False):
         super(EfficientNet, self).__init__()
 
         # yapf: disable
         settings = [
-
             # t,  c, n, s, k
+            # [1,  16, 1, 1, 3],  # MBConv1_3x3, SE, 112 -> 112
+            # [6,  24, 2, 2, 3],  # MBConv6_3x3, SE, 112 ->  56
+            # [6,  40, 2, 2, 5],  # MBConv6_5x5, SE,  56 ->  28   
+            # [6,  80, 3, 2, 3],  # MBConv6_3x3, SE,  28 ->  14   
+            # [6, 112, 3, 1, 5],  # MBConv6_5x5, SE,  14 ->  14
+            # [6, 192, 4, 2, 5],  # MBConv6_5x5, SE,  14 ->   7  
+            # [6, 320, 1, 1, 3]   # MBConv6_3x3, SE,   7 ->   7
+
+            #####
+            # for size 32 test 1
+            # [1,  16, 1, 1, 3],  # MBConv1_3x3, SE,  16 ->  16
+            # [6,  24, 2, 2, 3],  # MBConv6_3x3, SE,  16 ->   8   
+            # [6,  40, 2, 1, 5],  # MBConv6_5x5, SE,   8 ->   8   
+            # [6,  80, 3, 1, 3],  # MBConv6_3x3, SE,   8 ->   8  
+            # [6, 112, 3, 1, 5],  # MBConv6_5x5, SE,   8 ->   8  
+            # [6, 192, 4, 1, 5],  # MBConv6_5x5, SE,   8 ->   8   
+            # [6, 320, 1, 1, 3]   # MBConv6_3x3, SE,   8 ->   8
+            # for size 32 test 2
             [1,  16, 1, 1, 3],  # MBConv1_3x3, SE,  32 ->  32
             [6,  24, 2, 1, 3],  # MBConv6_3x3, SE,  32 ->  32   
             [6,  40, 2, 2, 5],  # MBConv6_5x5, SE,  32 ->  16   
@@ -208,6 +330,11 @@ class EfficientNet(nn.Module):
         # yapf: enable
 
         out_channels = _round_filters(32, width_mult)
+
+        # test 1 or size 224
+        # features = [ConvBNReLU(3, out_channels, 3, stride=2)]
+
+        # for size 32 ## test 2
         features = [ConvBNReLU(3, out_channels, 3, stride=1)]
         ##### 
 
@@ -220,11 +347,15 @@ class EfficientNet(nn.Module):
                 features += [MBConvBlock(in_channels, out_channels, expand_ratio=t, stride=stride, kernel_size=k)]
                 in_channels = out_channels
 
+        # remove 1280 layer
         last_channels = _round_filters(1280, width_mult)
         features += [ConvBNReLU(in_channels, last_channels, 1)]
+        # last_channels = in_channels
+
+        # replace Avepooling -> Conv
+        # self.Conv_last = nn.Conv2d(last_channels,last_channels, 8, groups = last_channels, bias=False)
 
         self.features = nn.Sequential(*features)
-        self.pool = nn.AvgPool2d(8)
         if coslinear:
             print('using coslinear')
             mid_channels = 200
@@ -244,6 +375,13 @@ class EfficientNet(nn.Module):
 
 
         for m in self.modules():
+            # if isinstance(m, nn.Conv2d):
+            #     nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            # elif isinstance(m, nn.BatchNorm2d):
+            #     m.weight.data.fill_(1.0)
+            #     m.bias.data.zero_()
+            # elif isinstance(m, nn.Linear):
+            #     nn.init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='linear')
 
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels  # fan-out
@@ -261,9 +399,10 @@ class EfficientNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        # x = x.mean([2, 3])
-        x = self.pool(x)
-        x = torch.squeeze(x)
+        # x = self.Conv_last(x).view(-1,1536)
+
+        x = x.mean([2, 3])
+        # print(x.size())
         x = self.classifier(x)
         return x
 
@@ -285,6 +424,30 @@ def _efficientnet(arch, pretrained, progress, **kwargs):
 def efficientnet_b0(pretrained=False, progress=True, **kwargs):
     return _efficientnet('efficientnet_b0', pretrained, progress, **kwargs)
 
+
+def efficientnet_b1(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet_b1', pretrained, progress, **kwargs)
+
+
+def efficientnet_b2(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet_b2', pretrained, progress, **kwargs)
+
+
 def efficientnet_b3(pretrained=False, progress=True, **kwargs):
     return _efficientnet('efficientnet_b3', pretrained, progress, **kwargs)
 
+
+def efficientnet_b4(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet_b4', pretrained, progress, **kwargs)
+
+
+def efficientnet_b5(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet_b5', pretrained, progress, **kwargs)
+
+
+def efficientnet_b6(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet_b6', pretrained, progress, **kwargs)
+
+
+def efficientnet_b7(pretrained=False, progress=True, **kwargs):
+    return _efficientnet('efficientnet_b7', pretrained, progress, **kwargs)
